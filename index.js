45dc5af47ed6bc463bb2198a45513813a81d039f
Michael Fuget <michaelfuget@gmail.com>
2021-02-09 07:22:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function fetchBooks() {
 }
 
 function renderBooks(json) {
-  const main = document.querySelector('main')
+  const main = document.querySelector('body')
   json.forEach(book => {
     const h2 = document.createElement('h2')
     h2.innerHTML = `<h2>${book.name}</h2>`
